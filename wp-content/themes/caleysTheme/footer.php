2b5ffbc5848ac6aa8ce6e0efe9b478ad7181d4b1
ADAM ANDREASON <adamandreason@gmail.com>
2021-05-05 22:33:00 -0600

    working on setting to wordpress

---
+++ wp-content/themes/caleysTheme/footer.php
@@ -0,0 +1,44 @@
+</main>
+    <div class="container social">
+        <a href="https://www.facebook.com/pg/CaleysCateringAndEvents" target="_blank">
+            <ion-icon name="logo-facebook"></ion-icon></a>
+        <a href="https://www.instagram.com/caleyscatering/?hl=en" target="_blank">
+            <ion-icon name="logo-instagram"></ion-icon>
+        </a>
+
+    </div>
+    <footer class="bg-dark">
+        <div class="footerFlex">
+        <p>PO Box 104 Draper, Utah 84020 • <a href="tel:801-231-5250">801-231-5250</a></p>        
+        <p>© 2021 TimeToady Sites • <a href="/wp-admin" target="_blank">Admin Login</a></p>
+        </div>
+
+        
+    </footer>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "image": "https://condescending-ride-11f5bc.netlify.app/assets/images/vaca3b.jpg",
+            "name": "Caley's Catering",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "PO Box 104 Draper",
+                "addressLocality": "Draper",
+                "addressRegion": "UT",
+                "postalCode": "84020",
+                "addressCountry": "US"
+            },
+            "telephone": "8012315250",
+            "openingHours": "Mo-Fr 10:00-19:00",
+            "priceRange": "Varies"
+        }
+    </script>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/less@3.13"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ wp-content/themes/caleysTheme/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caley's Catering and Events <?php wp_title(); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="container navContain">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+                <div class="container-fluid" id="theNav">
+                    <a class="navbar-brand" href="./index.html"><img class="theLogo" src="<?php echo get_template_directory_uri(); ?>/cayleys1.svg"
+                            alt="DreamVacations LLC"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+
+
+                </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <?php wp_nav_menu(array('menu' => 'primaryNav')) ?>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
